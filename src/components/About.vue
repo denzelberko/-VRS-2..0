7f2535d7e1d5221d65df62f0d640715462640bd8
NoahJMartins <noah.martins@mail.utoronto.ca>
2022-12-07 20:10:45 -0500

    About page finished

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,59 +3,23 @@
     <Header/>
     <div style="margin-left: 15%; margin-right: 15%;">
   <div class="wel">
-    <Welcome>Welcome to your all-in-one vacation guide!</Welcome>
+    <Welcome>This is a student project for MIE350 and should not be used for anything other than educational purposes.</Welcome>
   </div>
 
-  <div class="info">
-    <info>Using our own unique algorithm, we will find the perfect destination for all of your needs. With over 100+ locations to search from, your next trip is waiting. </info>
+  <div class="info2">
+    <b>Purpose</b><p>The purpose of this website is to create a unique tool to help users find their ideal vacation. Using a unique algorithm based on a variety of parameters, our team has created an effective method of finding your next destination.</p>
   </div>
+  <div class="info2"><b>Functionality</b><p>There are a broad set of functions that this website is capable of. Using the recommendations feature, based on your own personal parameters, the site will provide you with the top 5 destinations for you. 
+    Alternatively, the site can also be used to search from a variety of our included destinations by simply using a search bar. There, you will be able to find out more information about each location, including attractions and hotels.
+  You are also welcome to provide us with your own reviews, that other users will be able to see going forward.</p>
+  </div>
+  <div class="info2"><b>The Team</b><p>Our team consists of 6 Industrial Engineering students from the University of Toronto. Each with a unique set of skills, we have put our strengths together to create an innovative product. The individuals involved with the making of the site are: <br>Denzel the Front-End Wizard<br>Matthew the Algorithm King<br>Max the Report Maestro<br>Noah the Style Master<br>Pascal the Back-End Magician<br>Rai'd the Mobile Expert</p></div>
     
-    
-    <div style="position:relative; top:55px;">
-    <b-carousel
-      id="carousel-2"
-      :interval="4000"
-      controls
-      indicators
-      fade
-      img-width="512"
-      img-height="580"
-      style="text-shadow: 1px 1px 2px #333;"
-    >
-      <!-- Text slides with image -->
-
-
-      <b-carousel-slide
-        caption="Paris"
-        img-src="https://picsum.photos/2048/960/?image=318"
-      ></b-carousel-slide>
-
-      <b-carousel-slide
-        caption="San Francisco"
-        img-src="https://picsum.photos/2048/960/?image=392"
-      ></b-carousel-slide>
-
-      <b-carousel-slide
-        caption="Punta Cana"
-        img-src="https://picsum.photos/2048/960/?image=521"
-      ></b-carousel-slide>
-
-      <b-carousel-slide
-        caption="Florence"
-        img-src="https://picsum.photos/2048/960/?image=629"
-      ></b-carousel-slide>
-
-      <b-carousel-slide
-      caption="New York"  
-      img-src="https://picsum.photos/2048/960/?image=411"
-      >
-      </b-carousel-slide>
-
-    </b-carousel>
+  <div class="info2" ><b>Special Thanks</b><p>Thank you to Trivago for providing information as the basis for our destination data. </p><info>“Trivago.com - compare hotel prices worldwide,” trivago, 2022. [Online]. Available: https://www.trivago.com/. [Accessed: 07-Dec-2022]. </info>
   </div>
 
-  <div class="info">
-    <info>Visit <a href="http://localhost:3000/#/recommendations/">recommendations</a>  to find your next adventure! </info>
+  <div class="info2">
+    <info>What are you waiting for? Visit <a href="http://localhost:3000/#/recommendations/">recommendations</a>  to find your next adventure! </info>
   </div>
 </div>
   </div>
@@ -107,13 +71,22 @@ a {
 .info{
   color: #000000;
   padding: 20px 30px;
+  text-align: left;
+  text-decoration: none;
+  font-size: 20px;
+  position:relative; top:50px;
+}
+
+.info2{
+  color: #000000;
+  padding: 20px 30px;
   text-align: center;
   text-decoration: none;
   font-size: 20px;
   position:relative; top:50px;
 }
 Welcome {
-  font-size: 30px;
+  font-size: 20px;
   color: #0D0047;
   text-decoration: none;
   background-position: center;
